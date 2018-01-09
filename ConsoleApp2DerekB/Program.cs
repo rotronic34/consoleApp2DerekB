966381cc9623d19d5cf36b7ab570a9936bbf8bd8
Roeut Chak <roeut.chak@gmail.com>
2018-01-09 00:25:14 -0500

    initial commit starting with console.readLine()

---
+++ ConsoleApp2DerekB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2DerekB
+{
+    class MainClass 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("what is your namee?");
+            string name = Console.ReadLine();
+            Console.Write(name + " is cool");
+        
+        }
+    }
+}
