0ba7f88ad14bf8c4af9f497e3326d9da7fcd0eb2
Roeut Chak <roeut.chak@gmail.com>
2018-01-09 01:32:09 -0500

    reworking classes

--- ConsoleApp2DerekB/Program.cs
+++ ConsoleApp2DerekB/Program.cs
@@ -1,19 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp2DerekB
 {
-    class MainClass 
+    class Animal
     {
+        public double height { get; set; }
+        public double weight { get; set; }
+        public string sound { get; set; }
+
+        //To set your own setters and getters
+        //getters to limit access or setters to set certain values 
+        public string name;
+        public string Name {
+            get { return name; }
+            set { name = value; } // is very important, needs to be used for setting different items;
+        }
+        //once we have some attributes, we will need a way to initial thise animal object
+        //CONSTRUCTOR
+
+        public Animal(){
+            this.height = 0;
+            this.weight = 0;
+            this.sound = "no sound";
+            this.name = "no name";
+        }
+
+        public Animal(double height, double weight, string sound, string name){
+            this.height = height;
+            this.weight = weight;
+            this.sound = sound;
+            this.name = name;
+        }
+
+        static int numOfAnimals = 0;
+        //to get access to this static field need to create static method
+        public static int getNumOfAnimals() {
+            return numOfAnimals;
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("what is your namee?");
-            string name = Console.ReadLine();
-            Console.Write(name + " is cool");
-        
+
         }
     }
 }
