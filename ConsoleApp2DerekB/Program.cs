2bb524aea1cd48f62436056af37aa6588f88f555
Roeut Chak <roeut.chak@gmail.com>
2018-01-14 00:19:12 -0500

    ok

--- ConsoleApp2DerekB/Program.cs
+++ ConsoleApp2DerekB/Program.cs
@@ -12,14 +12,16 @@ namespace ConsoleApp2DerekB
         //To set your own setters and getters
         //getters to limit access or setters to set certain values 
         public string name;
-        public string Name {
+        public string Name
+        {
             get { return name; }
             set { name = value; } // is very important, needs to be used for setting different items;
         }
         //once we have some attributes, we will need a way to initial thise animal object
         //CONSTRUCTOR
 
-        public Animal(){
+        public Animal()
+        {
             this.height = 0;
             this.weight = 0;
             this.sound = "no sound";
@@ -27,7 +29,8 @@ namespace ConsoleApp2DerekB
             numOfAnimals++;
         }
 
-        public Animal(double height, double weight, string sound, string name){
+        public Animal(double height, double weight, string sound, string name)
+        {
             this.height = height;
             this.weight = weight;
             this.sound = sound;
@@ -35,21 +38,24 @@ namespace ConsoleApp2DerekB
             numOfAnimals++;
         }
 
-        static int numOfAnimals = 0;                
+        static int numOfAnimals = 0;
         //to get access to this static field need to create static method
-        public static int getNumOfAnimals() {
+        public static int getNumOfAnimals()
+        {
             return numOfAnimals;
         }
 
         //creating a couple other methods for animals to operate with
 
-        public string toString(){
-            return String.Format("{0} is {1} inches tall, weighs{2} lbs and likes to say {3}",name, height, weight, sound);
+        public string toString()
+        {
+            return String.Format("{0} is {1} inches tall, weighs{2} lbs and likes to say {3}", name, height, weight, sound);
         }
 
-        public int getSum(int num1 = 1, int num2 = 1){
+        public int getSum(int num1 = 1, int num2 = 1)
+        {
             return num1 + num2;
-            
+
         }
 
         public double getSum(double num1 = 1, double num2 = 1)
@@ -64,7 +70,7 @@ namespace ConsoleApp2DerekB
             Console.WriteLine("{0} says {1}", Spot.name, Spot.sound);
             Console.WriteLine(Spot.toString());
             Console.WriteLine("Num of animals: " + Animal.getNumOfAnimals());
-            Console.WriteLine(Spot.getSum(num2:5));
+            Console.WriteLine(Spot.getSum(num2: 5));
             Console.WriteLine(Spot.getSum(2.4));
 
             // create object using the obj initializer
@@ -90,15 +96,17 @@ namespace ConsoleApp2DerekB
     {
         public String favFood { get; set; }
 
-        public Dog() : base() {     //Constructor same name as class
+        public Dog() : base()
+        {     //Constructor same name as class
             this.favFood = "No favorite food";
         }
 
-        public Dog(double height, double weight, string name, string sound, string favFood) : base(height, weight, sound, name){
-           /* this.height = height;
-            this.weight = weight;
-            this.sound = sound;
-            this.name = name; */
+        public Dog(double height, double weight, string name, string sound, string favFood) : base(height, weight, sound, name)
+        {
+            /* this.height = height;
+             this.weight = weight;
+             this.sound = sound;
+             this.name = name; */
             this.favFood = favFood;
         }
 
@@ -109,4 +117,10 @@ namespace ConsoleApp2DerekB
         }
     }
 
+    //Abstract Class
+    abstract class Shape
+    {
+        public abstract double area();
+
+    }
 }
